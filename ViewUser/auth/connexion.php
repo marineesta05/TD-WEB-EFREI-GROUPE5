5ada8afd095429a1491e4e6f7f3ac2b3ee30edf4
marineesta05 <marineesta05@gmail.com>
2025-01-16 10:52:48 +0100

    Reorganisation des dossier

--- ViewUser/auth/connexion.php
+++ ViewUser/auth/connexion.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="/auth.css">
+<link rel="stylesheet" href="auth.css">
 <h1>Connexion</h1>
 <form action="/index.php?page=connexion" method="POST" >
     Email :<input type="email" name="email" required>
@@ -6,7 +6,7 @@
     Mot de passe :<input type="password" name="mdp" required>
     <br>
     <button type="submit">Se connecter</button>
-    <p>Toujours pas inscrit? <a href="/ViewUser/inscription.php">S'inscrire</a></p>
+    <p>Toujours pas inscrit? <a href="/ViewUser/auth/inscription.php">S'inscrire</a></p>
     <p>Je suis <a href="/ViewAdmin/connexionAdmin.php">admin</a></p>
 </form>
 
